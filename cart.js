3be785d5a8c72be84c591c44ed245f0e37832174
rujz <rujulotlikar@gmail.com>
2023-04-25 14:47:12 +0530

    added color shift

--- cart.js
+++ cart.js
@@ -1,6 +1,7 @@
-const COLOR_BTNS = document.querySelectorAll('.color');
+const COLOR_BTNS = document.querySelector('.btn-area');
 COLOR_BTNS.forEach(color => {
     color.addEventListener('click', () => {
+        console.log("clicked");
         let colorNameClass = color.className;
         if(!color.classList.contains('active-color')){
             let colorName = colorNameClass.slice(colorNameClass.indexOf('_') + 1, colorNameClass.length);
@@ -24,3 +25,4 @@ function setNewColor(color){
     document.querySelector('.box img').src = `D:\WT project\2_${color}.jpeg`;
     document.querySelector('.box img').src = `D:\WT project\3_${color}.jpeg`;
 }
+
